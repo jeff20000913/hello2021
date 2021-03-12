d2cd2525c941c2b5f266beb29de0fe8e4429253a
student <s1082826@gm.pu.edu.tw>
2021-03-12 14:58:58 +0800

    第二次commit

--- app/src/main/java/com/example/hello2021/MainActivity.kt
+++ app/src/main/java/com/example/hello2021/MainActivity.kt
@@ -8,6 +8,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        var txv:TextView = findViewById<TextView>(R.id.txv)
+        txv.text = "Hello tcyang"
+
 
     }
 }
